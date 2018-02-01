63a09504f9ee0f07c738160e8e1ec1130d5364c8
Ricardo Gosch <ricardogosch@gmail.com>
2018-02-01 14:27:17 -0200

    Challenge 14 - Fixed

--- challenge-14/challenge-14.js
+++ challenge-14/challenge-14.js
@@ -11,18 +11,10 @@
     Mostre esse array no console.
     */
     console.log( 'Number Objects Array:' );
-    var numberObjects = [
-        { number: 1 },
-        { number: 2 },
-        { number: 3 },
-        { number: 4 },
-        { number: 5 },
-        { number: 6 },
-        { number: 7 },
-        { number: 8 },
-        { number: 9 },
-        { number: 10 }
-    ];
+    var numberObjects = [];
+    for(var i = 1; i <= 10; i++){
+        numberObjects.push({ number: i });
+    }
     console.log(numberObjects);
 
     /*
@@ -91,7 +83,7 @@
     e atribuirá o seu nome invertido (usando o array criado acima).
     */
     console.log( '\nInversed Name:' );
-    var inversedName = name.reduce((initial, item) => {
+    var inversedName = name.reduceRight((initial, item) => {
         return initial + item;
     }, '');
     console.log(inversedName);
